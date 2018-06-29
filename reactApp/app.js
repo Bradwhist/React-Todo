fb491881446f6f3138e90df84fcfb28f8b585ff3
bradwhist <bradwhist@gmail.com>
2018-06-29 14:26:23 -0700

    checkpoint 2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Meow lives!</p>,
+
+let dummyData = (['probes', 'pylons', 'profit'])
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <ul>
+        {dummyData.map((task) => <Todo task={task} />)}
+      </ul>
+    );
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <li>
+        <p>{this.props.task}</p>
+        <button type="button">X</button>
+     </li>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'))
